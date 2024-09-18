129161f836f563529e4736f09cbe19f4c6598ba3
Starklord17 <maxi.sm117@gmail.com>
2024-09-18 18:55:30 -0300

    :zap: Alert

--- README.md
+++ README.md
@@ -46,3 +46,4 @@ Check out our [Next.js deployment documentation](https://nextjs.org/docs/deploym
 ## Components
 
 - https://ui.shadcn.com/docs/components/accordion
+- https://ui.shadcn.com/docs/components/alert
--- src/app/dashboard/alert/page.tsx
+++ src/app/dashboard/alert/page.tsx
@@ -1,7 +1,32 @@
+import { Alert, AlertTitle, AlertDescription } from "@/components/ui/alert";
+import { Terminal } from "lucide-react";
+
 export default function Page() {
   return (
-    <div>
-      <h1>Alert Page</h1>
+    <div className="grid gap-3">
+      <Alert>
+        <Terminal className="h-4 w-4" />
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          You can add components to your app using the cli.
+        </AlertDescription>
+      </Alert>
+
+      <Alert variant='destructive'>
+        <Terminal className="h-4 w-4" />
+        <AlertTitle>Destructive variant!</AlertTitle>
+        <AlertDescription>
+          You can add components to your app using the cli.
+        </AlertDescription>
+      </Alert>
+
+      <Alert variant='success'>
+        <Terminal className="h-4 w-4" />
+        <AlertTitle>Success custom variant!</AlertTitle>
+        <AlertDescription>
+          You can add components to your app using the cli.
+        </AlertDescription>
+      </Alert>
     </div>
   );
 }
