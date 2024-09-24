88d82de3e8881b5b9a52c6c3115ea34d0119d115
Starklord17 <maxi.sm117@gmail.com>
2024-09-24 18:45:20 -0300

    Refactor DataTable component and add custom cell renderers

--- src/app/dashboard/data-table/columns.tsx
+++ src/app/dashboard/data-table/columns.tsx
@@ -1,13 +1,33 @@
-"use client"
+"use client";
 
-import { Payment } from "@/data/payments.data"
-import { ColumnDef } from "@tanstack/react-table"
-
+import { Badge } from "@/components/ui/badge";
+import { Payment } from "@/data/payments.data";
+import { ColumnDef } from "@tanstack/react-table";
 
 export const columns: ColumnDef<Payment>[] = [
   {
     accessorKey: "status",
     header: "Status",
+    cell: ({ row }) => {
+      const status = row.getValue("status") as string;
+      const variant =
+        {
+          pending: "secondary",
+          processing: "info",
+          success: "success",
+          failed: "destructive",
+        }[status] ?? ("default" as any);
+
+      return (
+        <Badge
+          // className={`px-2 py-1 text-xs font-medium rounded-full ${color}`}
+          variant={variant}
+          capitalize
+        >
+          {status}
+        </Badge>
+      );
+    },
   },
   {
     accessorKey: "email",
@@ -15,10 +35,19 @@ export const columns: ColumnDef<Payment>[] = [
   },
   {
     accessorKey: "amount",
-    header: "Amount",
+    header: () => <div className="text-right">Amount</div>,
+    cell: ({ row }) => {
+      const amount = parseFloat(row.getValue("amount"));
+      const formatted = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(amount);
+
+      return <div className="text-right font-medium">{formatted}</div>;
+    },
   },
   {
     accessorKey: "clientName",
     header: "Client Name",
   },
-]
+];
