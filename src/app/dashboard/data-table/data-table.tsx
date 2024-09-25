5af2ccccc964202e6ac1bb4fb749220f8e22feeb
Starklord17 <maxi.sm117@gmail.com>
2024-09-25 12:43:14 -0300

    Refactor DataTable component and add row per page selection feature

--- src/app/dashboard/data-table/data-table.tsx
+++ src/app/dashboard/data-table/data-table.tsx
@@ -216,7 +216,7 @@ export function DataTable<TData, TValue>({
           </TableBody>
         </Table>
 
-        <div className="space-x-2 py-4 mx-2">
+        <div className="space-x-2 py-4 mx-2 flex justify-between items-center">
           <div className="flex-1 text-sm text-muted-foreground mt-2">
             {table.getFilteredSelectedRowModel().rows.length} of{" "}
             {table.getFilteredRowModel().rows.length} row(s) selected.
@@ -241,6 +241,26 @@ export function DataTable<TData, TValue>({
             </Button>
           </div>
         </div>
+
+        <Select
+          onValueChange={(value) => {
+            table.setPageSize(+value);
+          }}
+        >
+          <SelectTrigger className="w-[180px] m-2">
+            <SelectValue placeholder="10 Rows" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>Rows per page</SelectLabel>
+              <SelectItem value="10">10</SelectItem>
+              <SelectItem value="20">20</SelectItem>
+              <SelectItem value="30">30</SelectItem>
+              <SelectItem value="50">40</SelectItem>
+              <SelectItem value="100">100</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
       </div>
     </div>
   );
