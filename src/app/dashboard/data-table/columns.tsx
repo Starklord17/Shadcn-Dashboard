275a3dcdd80bddc6a9e208fc92bb2aa8052cd4c4
Starklord17 <maxi.sm117@gmail.com>
2024-09-25 10:43:30 -0300

    Refactor DataTable component and add row selection feature

--- src/app/dashboard/data-table/columns.tsx
+++ src/app/dashboard/data-table/columns.tsx
@@ -20,6 +20,7 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { toast } from "sonner";
+import { Checkbox } from "@/components/ui/checkbox"
 
 const SortedIcon = ({ isSorted }: { isSorted: false | SortDirection }) => {
   if (isSorted === "asc") {
@@ -34,6 +35,28 @@ const SortedIcon = ({ isSorted }: { isSorted: false | SortDirection }) => {
 };
 
 export const columns: ColumnDef<Payment>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
   {
     accessorKey: "clientName",
     header: ({ column }) => {
